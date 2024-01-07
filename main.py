a89b286aff4f01aa669fe311053e8fe635dfc682
Luca <lucanegresco@gmail.com>
2024-01-06 22:01:21 -0300

    nss

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def save_uploaded_file(uploaded_file):
     # Save the file to the 'uploads' folder
 
     file_path = os.path.join("uploads", uploaded_file.name)
-    with open(resource_path(file_path), "wb") as f:
+    with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
 
     return file_path

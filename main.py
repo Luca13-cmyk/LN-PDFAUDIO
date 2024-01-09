6efbc27fdab8f8dcf7939b9ba4b5d78ecec0901d
Luca <lucanegresco@gmail.com>
2024-01-09 13:49:26 -0300

    fixed file directory on linux

--- main.py
+++ main.py
@@ -1,3 +1,87 @@
+#
+#
+# import streamlit as st
+# import PyPDF2
+# import os
+# import pyttsx3
+# import sys
+#
+# convertingToAudio = "**Converting**..."
+#
+#
+# def resource_path(relative_path):
+#     """ Get absolute path to resource, works for dev and for PyInstaller """
+#     try:
+#         # PyInstaller creates a temp folder and stores path in _MEIPASS
+#         base_path = sys._MEIPASS2
+#     except Exception:
+#         base_path = os.path.abspath(".")
+#
+#     return os.path.join(base_path, relative_path)
+#
+#
+# def read_pdf(file_path):
+#     with open(file_path, "rb") as f:
+#         pdf_reader = PyPDF2.PdfReader(f)
+#         num_pages = len(pdf_reader.pages)
+#         text = ""
+#         for page_num in range(num_pages):
+#             page_text = pdf_reader.pages[page_num].extract_text()
+#             clean_text = page_text.strip().replace('\n', ' ')
+#             text += clean_text
+#     return text
+#
+# def text_to_audio(text, audio_file_path):
+#     engine = pyttsx3.init()
+#     engine.save_to_file(text, audio_file_path)
+#     engine.runAndWait()
+#
+# def save_uploaded_file(uploaded_file):
+#     # Save the file to the 'uploads' folder
+#
+#     file_path = os.path.join("uploads", uploaded_file.name)
+#     with open(file_path, "wb") as f:
+#         f.write(uploaded_file.getbuffer())
+#
+#     return file_path
+#
+#
+# def main():
+#     if not os.path.isdir("uploads"):
+#         os.mkdir("uploads")
+#
+#     global convertingToAudio
+#     st.title("PDF Converter to Audio")
+#
+#     # File uploader
+#     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+#
+#     if uploaded_file is not None:
+#         st.success("File successfully uploaded!")
+#
+#         # Save the uploaded file to the disk
+#         file_path = save_uploaded_file(uploaded_file)
+#
+#         # Display file content
+#         pdf_text = read_pdf(file_path)
+#
+#
+#         # Convert text to audio
+#
+#         # fix here
+#         audio_file_path = os.path.join("uploads", "output.mp3")
+#         st.markdown(convertingToAudio)
+#         text_to_audio(pdf_text, resource_path(audio_file_path))
+#         convertingToAudio = "Text converted to audio!"
+#         st.success(convertingToAudio)
+#
+#         # Provide a link-like button to the local audio file
+#         with open(resource_path(audio_file_path), 'rb') as f:
+#             st.download_button('Download Audio', f, file_name='output.mp3')
+#
+#
+# if __name__ == "__main__":
+#     main()
 
 
 import streamlit as st
@@ -6,13 +90,14 @@ import os
 import pyttsx3
 import sys
 
-convertingToAudio = "**Converting**..."
+convertingAudio = "**Converting**..."
 
+# linux
+# sudo apt install ffmpeg
+# sudo apt install espeak
 
 def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS2
     except Exception:
         base_path = os.path.abspath(".")
@@ -31,10 +116,6 @@ def read_pdf(file_path):
             text += clean_text
     return text
 
-def text_to_audio(text, audio_file_path):
-    engine = pyttsx3.init()
-    engine.save_to_file(text, audio_file_path)
-    engine.runAndWait()
 
 def save_uploaded_file(uploaded_file):
     # Save the file to the 'uploads' folder
@@ -42,16 +123,22 @@ def save_uploaded_file(uploaded_file):
     file_path = os.path.join("uploads", uploaded_file.name)
     with open(file_path, "wb") as f:
         f.write(uploaded_file.getbuffer())
-
     return file_path
 
 
+def text_to_audio(text, audio_file_path):
+    engine = pyttsx3.init()
+    engine.save_to_file(text, audio_file_path)
+    engine.runAndWait()
+
+
 def main():
+
     if not os.path.isdir("uploads"):
         os.mkdir("uploads")
 
-    global convertingToAudio
-    st.title("PDF Converter to Audio")
+    global convertingAudio
+    st.title("PDF Converter to audio")
 
     # File uploader
     uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
@@ -64,18 +151,23 @@ def main():
 
         # Display file content
         pdf_text = read_pdf(file_path)
-
+        st.text(pdf_text)
 
         # Convert text to audio
         audio_file_path = os.path.join("uploads", "output.mp3")
-        st.markdown(convertingToAudio)
+        st.markdown(convertingAudio)
         text_to_audio(pdf_text, resource_path(audio_file_path))
-        convertingToAudio = "Text converted to audio!"
-        st.success(convertingToAudio)
+        convertingAudio = "Text converted to audio"
+        st.success(convertingAudio)
 
-        # Provide a link-like button to the local audio file
-        with open(resource_path(audio_file_path), 'rb') as f:
-            st.download_button('Download Audio', f, file_name='output.mp3')
+        # Provide a link button to download the file
+        while True:
+            if os.path.isfile(resource_path(audio_file_path)):
+                with open(resource_path(audio_file_path), 'rb') as f:
+                    st.download_button("Download audio", f, file_name='output.mp3')
+                    break
+            else:
+                continue
 
 
 if __name__ == "__main__":
